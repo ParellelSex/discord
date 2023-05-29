dfaee942d45f8bc949342d6a688afbc28af9e088
ParellelSex <88294148+ParellelSex@users.noreply.github.com>
2023-05-28 20:36:10 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
 from PIL import Image, ImageFont, ImageDraw, ImageEnhance
 import web
-app = web.application(('(.*)', 'test'), globals())
+
+urls = (
+    '/(.*)', 'test'
+)
+app = web.application(urls, globals())
+
 img = Image.new("RGBA", (450, 135), (255, 255, 255, 255))
+
+if __name__ == "__main__":
+    app.run()
